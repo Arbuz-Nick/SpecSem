6c1d20518ba08c0c960f9399b9f714434367dd00
Nikolay Arbuzov <anr.qwert@gmail.com>
2022-12-02 15:10:37 +0300

    Add new kdtree: Start of rectangle

--- KDTree/kdtree.cpp
+++ KDTree/kdtree.cpp
@@ -1,144 +1,119 @@
-#include <vector>
+// A C++ program to demonstrate operations of KD tree
+#include <bits/stdc++.h>
 #include <iostream>
-
+#include <vector>
+using namespace std;
 
 const int k = 3;
-class KDPoint
-{
 
-public:
-    std::vector<float> dims;
-    KDPoint(float x_ = 0, float y_ = 0, float z_ = 0)
-    {
-        dims.push_back(x_);
-        dims.push_back(y_);
-        dims.push_back(z_);
-    }
-    KDPoint(std::vector<float> vector_)
-    {
-        for (int i = 0; i < k; i++)
-        {
-            dims.push_back(vector_[i]);
-        }
-    }
-    KDPoint(KDPoint &point_)
-    {
-        dims = point_.dims;
-    }
-
-    KDPoint &operator=(KDPoint const &point_)
-    {
-        if (this == &point_)
-            return *this;
-        this->dims = point_.dims;
-        return *this;
-    }
-};
-class Node
+// A structure to represent node of kd tree
+struct Node
 {
-public:
-    KDPoint point;
-    Node *left, *right;
-    Node()
-    {
-        left = nullptr;
-        right = nullptr;
-    }
-    Node(Node *node_)
-    {
-        left = node_->left;
-        right = node_->right;
-        point = node_->point;
-    }
-    Node operator=(Node const &node_)
-    {
-        if (this == &node_)
-            return *this;
-        this->point = node_.point;
-        this->left = node_.left;
-        this->right = node_.right;
-        return *this;
-    }
+	float point[k]; // To store k dimensional point
+	Node *left, *right;
 };
 
-Node newNode(KDPoint &point_)
+// A method to create a node of K D tree
+struct Node* newNode(float arr[])
 {
-    class Node temp;
+	struct Node* temp = new Node;
 
-    temp.point = point_;
+	for (int i=0; i<k; i++)
+	temp->point[i] = arr[i];
 
-    temp.left = temp.right = NULL;
-    return temp;
+	temp->left = temp->right = NULL;
+	return temp;
 }
 
-Node insertRec(Node *root, KDPoint &point, unsigned depth)
+// Inserts a new node and returns root of modified tree
+// The parameter depth is used to decide axis of comparison
+Node *insertRec(Node *root, float point[], unsigned depth)
 {
-    if (root == nullptr)
-        return &newNode(point);
+	// Tree is empty?
+	if (root == NULL)
+	return newNode(point);
 
-    unsigned cd = depth % k;
+	// Calculate current dimension (cd) of comparison
+	unsigned cd = depth % k;
 
-    if (point.dims[cd] < (root->point.dims[cd]))
-        *(root->left) = insertRec(root->left, point, depth + 1);
-    else
-        *(root->right) = insertRec(root->right, point, depth + 1);
+	// Compare the new point with root on current dimension 'cd'
+	// and decide the left or right subtree
+	if (point[cd] < (root->point[cd]))
+		root->left = insertRec(root->left, point, depth + 1);
+	else
+		root->right = insertRec(root->right, point, depth + 1);
 
-    return *root;
+	return root;
 }
 
-Node insert(Node &root, KDPoint &point)
+// Function to insert a new point with given point in
+// KD Tree and return new root. It mainly uses above recursive
+// function "insertRec()"
+Node* insert(Node *root, float point[])
 {
-    return insertRec(&root, point, 0);
+	return insertRec(root, point, 0);
 }
 
-bool arePointsSame(KDPoint &point1, KDPoint &point2)
+// A utility method to determine if two Points are same
+// in K Dimensional space
+bool arePointsSame(float point1[], float point2[])
 {
-    for (int i = 0; i < k; ++i)
-        if (point1.dims[i] != point2.dims[i])
-            return false;
+	// Compare individual pointinate values
+	for (int i = 0; i < k; ++i)
+		if (point1[i] != point2[i])
+			return false;
 
-    return true;
+	return true;
 }
 
-bool searchRec(Node *root, KDPoint &point, unsigned depth)
+// Searches a Point represented by "point[]" in the K D tree.
+// The parameter depth is used to determine current axis.
+bool searchRec(Node* root, float point[], unsigned depth)
 {
-    if (root == NULL)
-        return false;
-    if (arePointsSame(root->point, point))
-        return true;
+	// Base cases
+	if (root == NULL)
+		return false;
+	if (arePointsSame(root->point, point))
+		return true;
 
-    unsigned cd = depth % k;
+	// Current dimension is computed using current depth and total
+	// dimensions (k)
+	unsigned cd = depth % k;
 
-    if (point.dims[cd] < root->point.dims[cd])
-        return searchRec(root->left, point, depth + 1);
+	// Compare point with root with respect to cd (Current dimension)
+	if (point[cd] < root->point[cd])
+		return searchRec(root->left, point, depth + 1);
 
-    return searchRec(root->right, point, depth + 1);
+	return searchRec(root->right, point, depth + 1);
 }
 
-bool search(Node *root, KDPoint point)
+// Searches a Point in the K D tree. It mainly uses
+// searchRec()
+bool search(Node* root, float point[])
 {
-    return searchRec(root, point, 0);
+	// Pass current depth as 0
+	return searchRec(root, point, 0);
 }
 
+vector<float> inRectangle(Node* root, float rightTopFront, float leftBottomBack){}
+
+// Driver program to test above functions
 int main()
 {
-    struct Node *root = NULL;
-    int points[][k] = {{3, 6, 1}, {17, 15, 2}, {13, 15, 3}, {6, 12, 4}, {9, 1, 5}, {2, 7, 6}, {10, 19, 7}};
-    std::vector<KDPoint> point_vector;
-    for (int i = 0; i < sizeof(points) / sizeof(points[0]); ++i)
-    {
-        point_vector.emplace_back(point_vector[i, 0], point_vector[i, 1], point_vector[i, 2]);
-    }
-    for (auto point : point_vector)
-    {
-        root = &insert(*root, point);
-    }
+	struct Node *root = NULL;
+	float points[][k] = {{3, 6, 0}, {17, 15, 0}, {13, 15, 0}, {6, 12, 0},
+					{9, 1, 0}, {2, 7, 0}, {10, 19, 0}};
 
-    KDPoint point1(10, 19, 7);
-    (search(root, point1)) ? std::cout << "Found\n" : std::cout << "Not Found\n";
+	int n = sizeof(points)/sizeof(points[0]);
 
-    KDPoint point2(12, 19, 0);
-    (search(root, point2)) ? std::cout << "Found\n" : std::cout << "Not Found\n";
+	for (int i=0; i<n; i++)
+	root = insert(root, points[i]);
 
-    return 0;
+	float point1[] = {10, 19, 0};
+	(search(root, point1))? cout << "Found\n": cout << "Not Found\n";
+
+	float point2[] = {12, 19, 0};
+	(search(root, point2))? cout << "Found\n": cout << "Not Found\n";
+
+	return 0;
 }
