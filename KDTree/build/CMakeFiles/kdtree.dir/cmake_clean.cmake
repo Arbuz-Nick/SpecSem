20b02ec6c5819dd6e9cdca5e1812fc6d4c67df2d
Nikolay Arbuzov <anr.qwert@gmail.com>
2022-12-02 18:56:44 +0300

    Add all

---
+++ KDTree/build/CMakeFiles/kdtree.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kdtree.dir/sample.cpp.o"
+  "CMakeFiles/kdtree.dir/sample.cpp.o.d"
+  "kdtree"
+  "kdtree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kdtree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
